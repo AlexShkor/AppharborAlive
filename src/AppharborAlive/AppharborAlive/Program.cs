9ead2eb1d06d7942a28b2d68a5b91bcb1508f630
Alex Shkor <alex.shkor@gmail.com>
2013-04-05 21:17:27 +0300

    Initial commit

---
+++ src/AppharborAlive/AppharborAlive/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AppharborAlive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new WebClient();
+            var delay = TimeSpan.FromSeconds(int.Parse(ConfigurationManager.AppSettings["delay"]));
+            var urls = ConfigurationManager.AppSettings.AllKeys.Where(x=> x.StartsWith("site")).Select(x => ConfigurationManager.AppSettings[x]).ToList();
+            while (true)
+            {
+                foreach (var url in urls)
+                {
+                    try
+                    {
+                        client.DownloadString(new Uri(url));
+                    }
+                    catch(Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                }
+                Thread.Sleep(delay);
+            }
+        }
+    }
+}
